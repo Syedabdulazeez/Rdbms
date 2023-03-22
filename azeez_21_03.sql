3e3db099caa696e331e1f1def9a24be35f03dee9
syedabdulazeez <syed.abdulazeez@kreeti.com>
2023-03-22 17:31:32 +0530

    21-03 assignment added

--- azeez_21_03.sql
+++ azeez_21_03.sql
@@ -1,26 +1,41 @@
---1. Select all records in table “Employees” whose lastnamestarts with ‘Sah’
-select *
-from employees
-where last_name like 'sah%';
---2.Select 10 employees who were hired between 1stJ une,2021 to 15th February, 2022 .Start retrieving from the 4th recordinthetable
- select *
-from employees
-where hire_date BETWEEN  '2021-06-1' and '2022-12-15' 
-limit 10 offset 3;
---3.Find distinct top5 salary values (only salarycolumn),using alias for column as “Top5salaries”
-select distinct salary as Top5salaries
-from employees
-order by salary desc limit 5;
---4.find first_name,last_name and email of employees from the employee table  where salary lies between 25000 to 45000
-select first_name,last_name,email 
-from employees
-where salary between 25000 and 45000;
---5. write a quary to find first_name,manager_id and salary whwre the department_id is 101 and sort the result in ascending order of salary
-select first_name,manager_id,salary
-from employees
-where department_id =101
-order by salary asc;
---6. find the first_name that start with the letter "A", and find all the last_name that have "th" in  the  2nd and 3rd positions
-select *
-from employees
-where first_name like 'A%' and last_name like '_TH%';
+-- 1.Write the quary to get the department and deoartment wise total(sum) salary, display it in ascending order according to salary
+select departments.department_name,sum(employees.salary) as total_salarys
+from departments,employees
+where employees.department_id=departments.department_id
+group by departments.department_name
+order by total_salarys asc; 
+-- 2.write the quary to get the department,total no.employee of each department,total(sum)salary with respect to department from employee table
+select departments.department_name,count(employee_id) as NoOf_Employees,sum(employees.salary) as total_salarys
+from employees,departments
+where employees.department_id=departments.department_id
+group by departments.department_name;
+-- 3.get department wise maximum salary from employee table order by salary ascending order
+select departments.department_name,max(employees.salary) as max_salary
+from employees,departments
+where employees.department_id=departments.department_id
+group by departments.department_name
+order by max_salary;
+-- 4. Write a quary to get the departments where average salary is more then 60k
+select D.department_name,avg(E.salary) as average_slary
+from departments as D,employees as E
+where D.department_id=E.department_id
+group by D.department_name
+having avg(E.salary)>60000;
+-- 5.Write down the quary  to fetch  department name  asign to more  then one employee
+select d.department_name,count(e.employee_id) as number_of_employees
+from departments as d,employees as e
+where e.department_id=d.department_id
+group by d.department_name
+having count(e.employee_id)>1;
+-- 6.Write a query to show department_name and assignedTo where assignedTo
+-- will be “One candidate” if its assigned to only one employee otherwise
+-- “Multiple candidates”
+select d.department_name,
+case
+  when count(*)=1 
+    then 'One candidate'
+  else 'Multiple candidates' 
+end as assignedTo
+from employees as e,departments as d 
+where d.department_id=e.department_id
+group by d.department_name;
